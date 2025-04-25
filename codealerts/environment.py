076d6fdbf6f9b7d0c6c92414f3bc11771af9c34b
nicoacevedor <nacevedo@meteodata.cl>
2025-04-25 12:34:32 -0400

    Added creation of environment variables on a .env kind of file

---
+++ codealerts/environment.py
@@ -0,0 +1,39 @@
+from getpass import getpass
+from pathlib import Path
+from typing import Dict
+
+from dotenv import dotenv_values, set_key
+
+
+def load_env() -> Dict[str, str]:
+    env_file = Path().resolve() / ".notification_credentials"
+    if not env_file.exists():
+        create_env(env_file)
+    env_values = dotenv_values(env_file)
+    return env_values
+
+
+def create_env(env_file: Path) -> None:
+    email, password = '', ''
+    while email == '' or password == '':
+        print("Credentials setting:")
+        email = input("Email: ")
+        password_user = getpass()
+        confirm_password = getpass("Confirm password: ")
+        while password_user != confirm_password:
+            print("Incorrect password")
+            password_user = getpass()
+            confirm_password = getpass("Confirm password: ")
+        password = password_user
+    env_file.touch(mode=0o600, exist_ok=False)
+    set_key(
+        dotenv_path=env_file,
+        key_to_set="EMAIL",
+        value_to_set=email
+    )
+    set_key(
+        dotenv_path=env_file,
+        key_to_set="PASSWORD",
+        value_to_set=password
+    )
+    print("Credentials set!")
--- codealerts/notifications.py
+++ codealerts/notifications.py
@@ -1,18 +1,20 @@
 import smtplib
 from email.mime.text import MIMEText
-from typing import Any, Callable
+
+from .environment import load_env
 
 
 def send_email(subject: str, body: str) -> None:
-    from_email = "nacevedo@meteodata.cl"
-    from_password = "bdnu lelu vddi vxgf"
+    env = load_env()
+    # from_email = "nacevedo@meteodata.cl"
+    # from_password = "bdnu lelu vddi vxgf"
 
     msg = MIMEText(body)
     msg["Subject"] = subject
-    msg["From"] = from_email
-    msg["To"] = from_email
+    msg["From"] = env["EMAIL"]
+    msg["To"] = env["EMAIL"]
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login(from_email, from_password)
+        smtp.login(env["EMAIL"], env["PASSWORD"])
         smtp.send_message(msg)
 
